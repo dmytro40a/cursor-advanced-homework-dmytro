b97cce13386bceee695b35e5ef8b6181767315f1
dmytro40a <extremesoroka@gmail.com>
2022-08-23 22:28:12 +0300

    Fixed homework #1

--- 01-homework/script.js
+++ 01-homework/script.js
@@ -4,24 +4,25 @@ let productClothes = 123.965;
 let productAccessories = 90.2345;
 
 // MAX
-console.log(Math.max(productBoots, productClothes, productAccessories));
+let productSumMax = Math.max(productBoots, productClothes, productAccessories);
+console.log(productSumMax);
 
 // MIN
-console.log(Math.min(productBoots, productClothes, productAccessories));
+let productSumMin = Math.min(productBoots, productClothes, productAccessories);
+console.log(productSumMin);
 
 // SUM
 let productSum = productBoots + productClothes + productAccessories;
 console.log(productSum);
 
 // FLOOR
-let productFloor = Math.floor(
-  productBoots + productClothes + productAccessories
-);
+let productFloor = Math.floor(productSum);
 console.log(productFloor);
 
 // SUM ROUND
 let productSumRounded = Math.round(productSum / 100) * 100;
-console.log(Math.round(productSumRounded));
+let productSumRound = Math.round(productSumRounded);
+console.log(productSumRound);
 
 // BOOLEAN
 let productSumFloor = Math.floor(productSum / 100) * 100;
@@ -29,42 +30,38 @@ let productSumBoolean = productSumFloor % 2 == 0;
 console.log(productSumBoolean);
 
 //SUM 500 UAH
-console.log(500 - productSum);
+let productAmount = 500 - productSum;
+console.log(productAmount);
 
 //TO FIXED
-console.log(+(productSum / 3).toFixed(2));
+let productSumFixed = +(productSum / 3).toFixed(2);
+console.log(productSumFixed);
 
 //RANDOM SALE
-let productSale = productSum - Math.random();
+let productDiscount = productSum * Math.max(0.3);
+let productSale = productSum - Math.random() * productDiscount;
 console.log(productSale);
 
 //RANDOM SALE ROUND
-console.log(+productSale.toFixed(2));
+let productSaleRound = +productSale.toFixed(2);
+console.log(productSaleRound);
 
 //SALE PROFIT
 let productSaleProfit = productSum / 2;
-let productDiscount = productSum * 0.3;
-console.log(productSaleProfit - productDiscount);
+let productSumProfit = productSaleProfit - productDiscount;
+console.log(productSumProfit);
 
 //ADVANCED
 console.log(`
-  Максимальна ціна: ${Math.max(
-    productBoots,
-    productClothes,
-    productAccessories
-  )}
-  Мінімальна ціна ${Math.min(productBoots, productClothes, productAccessories)}
+  Максимальна ціна: ${productSumMax}
+  Мінімальна ціна ${productSumMin}
   Вартість всіх товарів: ${productSum}
   Округлена в меншу сторону сума всіх товарів: ${productFloor}
-  Сума товарів округлена до сотень: ${Math.round(productSumRounded)}
+  Сума товарів округлена до сотень: ${productSumRound}
   Чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом? ${productSumBoolean}
-  Сума решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.: ${
-    500 - productSum
-  }
-  Середнє значення цін: ${+(productSum / 3).toFixed(2)}
+  Сума решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.: ${productAmount}
+  Середнє значення цін: ${productSumFixed}
   Сума до сплати з випадковою знижкою
   ${productSale}
-  Випадкова знижка та округлена сума до оплати: ${+productSale.toFixed(2)}
-  Чистий прибуток, якщо клієнт заплатив зі знижкою 30%: ${
-    productSaleProfit - productDiscount
-  }`);
+  Випадкова знижка та округлена сума до оплати: ${productSaleRound}
+  Чистий прибуток, якщо клієнт заплатив зі знижкою 30%: ${productSumProfit}`);
