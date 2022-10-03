18a7993f3e7729a2a4507918969dd8602a839479
dmytro40a <extremesoroka@gmail.com>
2022-10-03 03:19:47 +0300

    Added HW #9, #10

---
+++ 09-homework/script.js
@@ -0,0 +1,22 @@
+document.body.style.background = generateRandomColor();
+
+function generateBlocks() {
+  const container = document.querySelector(".container-box");
+  let boxNumber = 25;
+  container.classList.add("container");
+  container.innerHTML = "";
+
+  for (let i = 0; i < boxNumber; i++) {
+    const createBox = document.createElement("div");
+    createBox.classList.add("box");
+    createBox.style.backgroundColor = generateRandomColor();
+    container.append(createBox);
+  }
+}
+
+function generateRandomColor() {
+  const numberOfColors = 9999;
+  return `#${Math.floor(Math.random() * numberOfColors).toString(16)}`;
+}
+generateBlocks();
+setInterval(generateBlocks, 1000);
